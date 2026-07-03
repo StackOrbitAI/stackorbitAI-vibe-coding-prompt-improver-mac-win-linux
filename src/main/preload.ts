37d193ee38230578ef3be616a4b0c2a501922f2c
Mohd Akeel <khanhostinger@gmail.com>
2026-07-03 18:02:29 +0000

    fix: Push complete source files for v1.0.3

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -5,6 +5,9 @@ contextBridge.exposeInMainWorld('api', {
   saveSettings: (settings: any) => ipcRenderer.invoke('save-settings', settings),
   getModels: (provider: string) => ipcRenderer.invoke('get-models', provider),
   closeSettings: () => ipcRenderer.send('close-settings'),
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+  openExternalUrl: (url: string) => ipcRenderer.send('open-external-url', url),
   onHUDUpdate: (callback: (data: any) => void) => {
     const listener = (_event: any, data: any) => callback(data);
     ipcRenderer.on('hud-update', listener);
