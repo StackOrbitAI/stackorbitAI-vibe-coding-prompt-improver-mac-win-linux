68c73e71bbafcbaac26c4a45e623674580d4246e
Mohd Akeel <khanhostinger@gmail.com>
2026-07-03 18:45:16 +0000

    fix: Add windowsHide: true to prevent CMD window popup

--- src/main/services/keyboard.ts
+++ src/main/services/keyboard.ts
@@ -16,17 +16,16 @@ export function simulateCopy(): Promise<void> {
   return new Promise((resolve) => {
     if (process.platform === 'win32') {
       const exePath = getSendKeysPath();
-      execFile(exePath, ['^c'], () => {
-        // We resolve regardless of errors to ensure the flow isn't permanently blocked
+      execFile(exePath, ['^c'], { windowsHide: true }, () => {
         resolve();
       });
     } else if (process.platform === 'darwin') {
-      exec(`osascript -e 'delay 0.1' -e 'tell application "System Events" to keystroke "c" using {command down}'`, () => {
+      exec(`osascript -e 'delay 0.1' -e 'tell application "System Events" to keystroke "c" using {command down}'`, { windowsHide: true }, () => {
         resolve();
       });
     } else {
       // Linux
-      exec('xdotool key ctrl+c', () => {
+      exec('xdotool key ctrl+c', { windowsHide: true }, () => {
         resolve();
       });
     }
@@ -40,16 +39,16 @@ export function simulatePaste(): Promise<void> {
   return new Promise((resolve) => {
     if (process.platform === 'win32') {
       const exePath = getSendKeysPath();
-      execFile(exePath, ['^v'], () => {
+      execFile(exePath, ['^v'], { windowsHide: true }, () => {
         resolve();
       });
     } else if (process.platform === 'darwin') {
-      exec(`osascript -e 'delay 0.1' -e 'tell application "System Events" to keystroke "v" using {command down}'`, () => {
+      exec(`osascript -e 'delay 0.1' -e 'tell application "System Events" to keystroke "v" using {command down}'`, { windowsHide: true }, () => {
         resolve();
       });
     } else {
       // Linux
-      exec('xdotool key ctrl+v', () => {
+      exec('xdotool key ctrl+v', { windowsHide: true }, () => {
         resolve();
       });
     }
