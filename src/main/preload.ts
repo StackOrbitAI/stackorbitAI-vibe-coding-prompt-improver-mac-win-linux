767be45ca2e4b44227213e3c043c49e767242761
Mohd Akeel <khanhostinger@gmail.com>
2026-07-03 19:56:51 +0000

    feat: Expose prepareUpdateExit bridge

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -7,6 +7,7 @@ contextBridge.exposeInMainWorld('api', {
   closeSettings: () => ipcRenderer.send('close-settings'),
   getAppVersion: () => ipcRenderer.invoke('get-app-version'),
   checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+  prepareUpdateExit: () => ipcRenderer.send('prepare-update-exit'),
   openExternalUrl: (url: string) => ipcRenderer.send('open-external-url', url),
   onHUDUpdate: (callback: (data: any) => void) => {
     const listener = (_event: any, data: any) => callback(data);
